3dd3fb1f8a4f5ef08629c981d6412dadf22d40c8
hadim8455 <hadim8455@gmail.com>
2025-02-13 06:52:30 +0500

    Ass 3

--- app.js
+++ app.js
@@ -1,3 +1,86 @@
-                // Assignment # 01
+                // Assignments # 01
+ 
+// 1.
+// alert("Error! Please enter a valid password.")
+
+
+// 2. 
+//  alert("Welcome to JS Land...\n Happy Coding!")
+
+// 3.
+// alert("Welcome to JS Land...")
+
+// 4.
+// alert("Happy Coding!")
+
+// 5.
+// alert("Hello... I can run JS through my web browser's console")
+// console.log(alert)
+
+// 6.
+// yea html file mein bana huwa hai
 
                 
+                // Assignments # 02
+
+// 1.
+// var userName = "hadim8455"
+// console.log(userName)
+
+// 2.
+// var firstName = "Muhammad "
+// var lastName = "Hadi"
+
+// var fullName = firstName + lastName
+// console.log(fullName)
+
+// 3.
+// var message = "Hello World"
+// alert(message)
+
+// 4.
+// var name = alert("M-Hadi")
+// var age = alert("23")
+// var course = alert("Modern Mobile Application Development")
+
+
+// 5.
+// var demo = alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// 6.
+// var email = "My Email Address is "
+// var Address = "hadim8455@gmail.com"
+// var emailAddress = alert(email+Address)
+
+// 7.
+// var book = alert("A smart way to learn Javascript")
+
+// 8.
+// var demo = "Yah! I can write HTML content through JavaScript"
+// document.write(demo)
+
+// 9.
+// var sample = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// document.write(sample)
+
+
+                // Assignments # 03
+
+// 1.               
+// var age = "I am 23 years old "
+// alert(age)
+
+// 2.
+// var demo = "You have visited this site 15 times"
+// alert(demo)
+
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+// var birthYear = "My birth year is 19 jan 2002 \n Data type of my declared variable is number"
+// document.write(birthYear)
+
+
+
+
