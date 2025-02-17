9fdea16a096e25b508a2db219bb1ed392883be81
hadim8455 <hadim8455@gmail.com>
2025-02-17 21:29:24 +0500

    Ass No 5 Q#7

--- app.js
+++ app.js
@@ -143,13 +143,197 @@
 // e) Variable names should not be JS _________
 
 
-document.write("<b>Rules for naming JS variables</b>")
+// document.write("<b>Rules for naming JS variables</b>")
 
-document.write("<br/><br/>")
-document.write("a. Variable names can only contain  Letters, $ and  _ . For example $my_1stVariable")
-document.write("<br/>")
-document.write("b. Variables must begin with a Letters, _ or $. For example $name, _name or name")
-document.write("<br/>")
-document.write("c. Variable names are case sensitive")
-document.write("<br/>")
-document.write("d. Variable names should not be JS keywords")
+// document.write("<br/><br/>")
+// document.write("a. Variable names can only contain  Letters, $ and  _ . For example $my_1stVariable")
+// document.write("<br/>")
+// document.write("b. Variables must begin with a Letters, _ or $. For example $name, _name or name")
+// document.write("<br/>")
+// document.write("c. Variable names are case sensitive")
+// document.write("<br/>")
+// document.write("d. Variable names should not be JS keywords")
+
+
+
+
+
+                                    // Assignments # 05
+
+
+
+
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+
+// Answer No 01
+
+// var num1 = 20
+// var num2 = 30
+// var add = num1 + num2
+// document.write("Num1: ", num1)
+// document.write("<br>")
+// document.write("Num2: ", num2)
+// document.write("<br>")
+// document.write("Add: ", add)
+
+
+// 2. Repeat task1 for subtraction, multiplication, division &
+// modulus.
+
+// Answer No 02
+
+// var num1 = 21
+// var num2 = 20
+// var sub = num1 - num2
+// document.write("The sub of 21 and 20   ==>  ",sub)
+
+
+// var num1 = 21
+// var num2 = 20
+// var mul = num1 * num2
+// document.write("The mul of 21 and 20   ==>  ",mul)
+
+// var num1 = 21
+// var num2 = 20
+// var div = num1 / num2
+// document.write("The div of 21 and 20   ==>  ",div)
+
+// var num1 = 21
+// var num2 = 20
+// var mod = num1 % num2
+// document.write("The mod of 21 and 20   ==>  ",mod)
+
+
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value by 3.
+// l. Output : “The remainder is : 0”.
+
+
+// Answer No 03
+
+// var a = "Value after variable declaration is: ??";
+// document.write(a , "<br>");
+
+// a = "10";
+// document.write("Initial Value: ", a, "<br>");
+
+// a++
+// document.write("Value After Increment: ", a, "<br/>")
+
+// a = a + 8
+// document.write("Value After 8 Add : ", a, "<br/>")
+
+// a--
+// document.write("Value After decrement: ", a, "<br/>")
+
+// a = a % 3
+// document.write("The remainder is: ", a, "<br/>")
+
+
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie. Example output:
+
+
+// Answer No 04
+
+// var a = 600  
+// document.write("Cost of one ticket is: ", a," PKR", "<br/>");
+
+// var a = a*5
+// document.write("Cost of five ticket is: ", a," PKR", "<br/>");
+
+
+// 5. Write a script to display multiplication table of any
+// number in your browser. E.g
+
+// Answer No 05
+
+// var a = 4
+// document.write("<b>Write Table Of 2</b>", "<br/>", "<br/>")
+// document.write("4 x 1 = ", a * 1, "<br/>")
+// document.write("4 x 2 = ", a * 2, "<br/>")
+// document.write("4 x 3 = ", a * 3, "<br/>")
+// document.write("4 x 4 = ", a * 4, "<br/>")
+// document.write("4 x 5 = ", a * 5, "<br/>")
+// document.write("4 x 6 = ", a * 6, "<br/>")
+// document.write("4 x 7 = ", a * 7, "<br/>")
+// document.write("4 x 8 = ", a * 8, "<br/>")
+// document.write("4 x 9 = ", a * 9, "<br/>")
+// document.write("4 x 10 = ", a * 10, "<br/>")
+
+
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+
+
+// Answer No 06
+
+// var c = 25
+// var f = (c * 9 / 5) + 32;
+// document.write("25&#8451; Convert In fahrenheit is equal to: ", f,"&#8457;","</br>")
+
+// var f = 70
+// var c = (f - 32) * 5/9;
+// document.write("70&#8457; Convert In Celsius is equal to: ", c,"&#8451;")
+
+
+// 7. Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables.
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
+// Compute the total cost & show the receipt in your browser.
+
+
+// Answer No 07
+
+// document.write("<b> Shopping Cart </b>", "<br/>", "<br/>")
+
+// var item1 = 250
+// document.write("Cost Of Item 1 is: ", item1, "<br/>")
+
+// document.write("Quantity Of Item 1 is: ", 3, "<br/>")
+// var Qty1 = item1*3
+
+// var item2 = 720
+// document.write("Cost Of Item 2 is: ", item2, "<br/>")
+
+// document.write("Quantity Of Item 1 is: ", 3, "<br/>")
+// var Qty2 = item2*2
+
+// var shipping = 250
+// document.write("Shipping Charges: ", shipping, "<br/>", "<br/>", "<br/>")
+
+// var totalCost = Qty1 + Qty2 + shipping
+// document.write("Total Cost Of Your Products: ", totalCost)
+
+
+
+
+
+
+
+
+
+
+
+
